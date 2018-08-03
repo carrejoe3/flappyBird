988a7e45055aff64bef90073e483a3cf8554a897
Joe Carre <joe.carre@rfa.co.uk>
2018-08-03 17:06:54 +0100

    increment score if fly through pipe

--- sprite.js
+++ sprite.js
@@ -1,4 +1,4 @@
-var 
+var
 
 s_bird,
 s_bg,
@@ -32,14 +32,14 @@ function initSprites(img) {
 		new Sprite(img, 156, 128, 17, 12),
 		new Sprite(img, 156, 141, 17, 12)
 	];
-	
+
 	s_bg = new Sprite(img,   0, 0, 138, 114);
 	s_bg.color = "#70C5CF";
 	s_fg = new Sprite(img, 138, 0, 112,  56);
-	
+
 	s_pipeNorth = new Sprite(img, 251, 0, 26, 200);
 	s_pipeSouth = new Sprite(img, 277, 0, 26, 200);
-	
+
 	s_text = {
 		FlappyBird: new Sprite(img, 59, 114, 96, 22),
 		GameOver:   new Sprite(img, 59, 136, 94, 19),
@@ -60,9 +60,14 @@ function initSprites(img) {
 	s_numberS = new Sprite(img, 0, 177, 6,  7);
 	s_numberB = new Sprite(img, 0, 188, 7, 10);
 
-	s_numberS.draw = s_numberB.draw = function(ctx, x, y, num) {
+	s_numberS.draw = s_numberB.draw = function(ctx, x, y, num, center) {
 		num = num.toString();
 		var step = this.width + 2;
+
+		if(center) {
+			x = center - (num.length * step - 2) / 2;
+		}
+
 		for (var i = 0, len = num.length; i < len; i++) {
 			var n = parseInt(num[i]);
 			ctx.drawImage(img, step*n, this.y, this.width, this.height,
